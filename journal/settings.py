ebd985461dae8d5a8464733a7d6b680af6a56783
UnicornSymbol <623882484@qq.com>
2017-10-26 19:35:46 +0800

    add requirements

--- journal/settings.py
+++ journal/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'mytest',
+        'NAME':'journal',
         'USER':'szop',
         'PASSWORD':'szop',
-        'HOST':'172.30.1.61',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
     }
 }
